7c94356e2f9718c75b64e7ae9250a871175018d9
harbargerdev <aharbarger@gmail.com>
2019-01-01 22:29:12 -0500

    Initial Solution Commit for front-end

---
+++ CodeIt.Photography.Presenter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeIt.Photography.Presenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
