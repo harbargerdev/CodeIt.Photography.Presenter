814577f6d15e329f90248124c6e033810ecdf1a5
Andrew H <aharbarger@gmail.com>
2019-01-04 16:32:36 -0500

    Skeleton of the DALC

---
+++ CodeIt.Photography.Libraries/DataAccessLayer/PostDALC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeIt.Photography.Libraries.DataAccessLayer
+{
+    public class PostDALC
+    {
+        public string ConnectionString
+        {
+            get { return string.Empty; }
+        }
+    }
+}
