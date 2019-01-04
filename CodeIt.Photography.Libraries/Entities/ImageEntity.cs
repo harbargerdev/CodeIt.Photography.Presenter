b1f2b234a6eeb791e115b9d0ef3e85d45c8e457f
Andrew H <aharbarger@gmail.com>
2019-01-04 14:59:52 -0500

    Added common entities

---
+++ CodeIt.Photography.Libraries/Entities/ImageEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeIt.Photography.Libraries.Entities
+{
+    public class ImageEntity
+    {
+        #region Properties
+
+        public string ImageId { get; set; }
+        public string ImageTitle { get; set; }
+        public string ThumbnailDownloadLink { get; set; }
+        public string WatermarkedDownloadLink { get; set; }
+        public string Caption { get; set; }
+        public string Location { get; set; }
+
+        #endregion
+    }
+}
---
+++ CodeIt.Photography.Libraries/Entities/PostEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeIt.Photography.Libraries.Entities
+{
+    [Serializable]
+    public class PostEntity
+    {
+        #region Properties
+
+        public string PostId { get; set; }
+        public string PostTitle { get; set; }
+        public string Summary { get; set; }
+        public string Body { get; set; }
+        public List<ImageEntity> Images { get; set; }
+
+        #endregion
+    }
+}
