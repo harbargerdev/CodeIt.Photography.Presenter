df0cc5e5acdd7fd2a0eea8d0705dc2a6870057aa
harbargerdev <aharbarger@gmail.com>
2019-01-07 07:03:04 -0500

    Starting some implementation

---
+++ CodeIt.Photography.Presenter.WebAPI/Controllers/BlogPostController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeIt.Photography.Libraries.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CodeIt.Photography.Presenter.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetBlogPost(int id)
+        {
+            JsonResult json = null;
+
+            try
+            {
+                BlogPostEntity blogPost = new BlogPostEntity();
+
+                json = new JsonResult(JsonConvert.SerializeObject(blogPost));
+                json.StatusCode = 200;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return json;
+        }
+    }
+}
