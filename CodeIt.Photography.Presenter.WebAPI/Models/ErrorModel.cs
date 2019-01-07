0f4e5b3bce88f45aa1c69d6468a0739541d9d485
harbargerdev <aharbarger@gmail.com>
2019-01-07 07:02:51 -0500

    Added ErrorModel Class

---
+++ CodeIt.Photography.Presenter.WebAPI/Models/ErrorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeIt.Photography.Presenter.WebAPI.Models
+{
+    public enum ErrorType { DatabaseException = 0, MissingArgument, InternalException }
+
+    [Serializable]
+    public class ErrorModel
+    {
+
+    }
+}
